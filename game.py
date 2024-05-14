c78691ded89321c8af282719b226ceb86e043023
Joshua <joshuakijkuit@hotmail.com>
2024-05-14 17:59:04 +0200

    Fixed end game screen, added game reset

--- game.py
+++ game.py
@@ -22,9 +22,10 @@ class Game:
         self.destroyed_tires = Tire("DESTROYED", 0.0, 2.0, -0.2, 0.005, -0.1, -0.05, 0.5)
 
         # Game variables
-        self.player = Car(pg.transform.scale(pg.image.load("res/textures/car_merc.png"), (60, 21)).convert_alpha(),
-                          [500, 300], self.soft_tires)
-        self.computer = Car(pg.transform.scale(pg.image.load("res/textures/car_alfa.png"), (60, 21)).convert_alpha(),
+        self.car_skins = ("car_alfa.png", "car_aston.png", "car_bull.png", "car_mclaren.png", "car_merc.png")
+        self.player = Car(pg.transform.scale(pg.image.load("res/textures/" + random.choice(self.car_skins)), (60, 21)).convert_alpha(),
+                          [500, 300], self.medium_tires)
+        self.computer = Car(pg.transform.scale(pg.image.load("res/textures/" + random.choice(self.car_skins)), (60, 21)).convert_alpha(),
                             [520, 340], self.medium_tires)
 
         self.a_down = False
@@ -90,6 +91,18 @@ class Game:
         self.player.tires = new_tires
         self.player.distance_driven = 0.0
 
+    def reset_game(self):
+        self.player = Car(pg.transform.scale(pg.image.load("res/textures/" + random.choice(self.car_skins)), (60, 21)).convert_alpha(),
+                          [500, 300], self.medium_tires)
+        self.computer = Car(pg.transform.scale(pg.image.load("res/textures/" + random.choice(self.car_skins)), (60, 21)).convert_alpha(),
+                            [520, 340], self.medium_tires)
+        self.a_down = False
+        self.d_down = False
+        self.w_down = False
+        self.s_down = False
+        self.pit_timer = 0.0
+        self.barricade_collision = False
+
     def start(self):
         play_button = pg.Rect((265, 375), (750, 100))
         quit_button = pg.Rect((265, 500), (750, 100))
@@ -118,48 +131,48 @@ class Game:
                         pg.mixer.Sound.play(self.click_sound)
                         self.game()
 
-            # Render
-            self.screen.fill(self.bg_color)
+                # Render
+                self.screen.fill(self.bg_color)
 
-            # Grass background
-            green_background = pg.Rect((0, 400), (self.screen.get_width(), 400))
-            pg.draw.rect(self.screen, self.color_green, green_background)
+                # Grass background
+                green_background = pg.Rect((0, 400), (self.screen.get_width(), 400))
+                pg.draw.rect(self.screen, self.color_green, green_background)
 
-            # Road
-            road_background = pg.Rect((340, 400), (600, 400))
-            pg.draw.rect(self.screen, self.color_black, road_background)
-            # Road side - Left
-            pg.draw.polygon(self.screen, self.color_black, ((340, 400), (340, 720), (250, 720)))
-            # Road side - Right
-            pg.draw.polygon(self.screen, self.color_black, ((940, 400), (940, 720), (1035, 720)))
-            # Road side wall - left
-            pg.draw.polygon(self.screen, self.color_red, ((340, 400), (250, 720), (210, 720), (300, 400)))
-            # Road side wall - right
-            pg.draw.polygon(self.screen, self.color_red, ((940, 400), (1035, 720), (1075, 720), (980, 400)))
+                # Road
+                road_background = pg.Rect((340, 400), (600, 400))
+                pg.draw.rect(self.screen, self.color_black, road_background)
+                # Road side - Left
+                pg.draw.polygon(self.screen, self.color_black, ((340, 400), (340, 720), (250, 720)))
+                # Road side - Right
+                pg.draw.polygon(self.screen, self.color_black, ((940, 400), (940, 720), (1035, 720)))
+                # Road side wall - left
+                pg.draw.polygon(self.screen, self.color_red, ((340, 400), (250, 720), (210, 720), (300, 400)))
+                # Road side wall - right
+                pg.draw.polygon(self.screen, self.color_red, ((940, 400), (1035, 720), (1075, 720), (980, 400)))
 
-            # Play button and text
-            pg.draw.rect(self.screen, self.color_dark_grey, play_button, border_radius=15)
-            if play_button.collidepoint(pg.mouse.get_pos()):
-                play_text = self.font_50.render("Play", False, self.color_red).convert()
-            else:
-                play_text = self.font_50.render("Play", False, self.color_white).convert()
-            self.screen.blit(play_text, play_text.get_rect(center=(play_button.centerx, play_button.centery)))
-            # Quit button and text
-            pg.draw.rect(self.screen, self.color_dark_grey, quit_button, border_radius=15)
-            if quit_button.collidepoint(pg.mouse.get_pos()):
-                quit_text = self.font_50.render("Quit", False, self.color_red).convert()
-            else:
-                quit_text = self.font_50.render("Quit", False, self.color_white).convert()
-            self.screen.blit(quit_text, quit_text.get_rect(center=(quit_button.centerx, quit_button.centery)))
-            # Logo
-            self.screen.blit(self.logo_small, self.logo_small.get_rect(center=((self.window_width / 2) - 155, 92)))
-            # Title text
-            self.screen.blit(self.title_text, (540, 70))
-            # Credit text
-            self.screen.blit(self.credit_text, (300, 650))
-            # PyGame Render
-            pg.display.update()
-            self.clock.tick(self.fps)
+                # Play button and text
+                pg.draw.rect(self.screen, self.color_dark_grey, play_button, border_radius=15)
+                if play_button.collidepoint(pg.mouse.get_pos()):
+                    play_text = self.font_50.render("Play", False, self.color_red).convert()
+                else:
+                    play_text = self.font_50.render("Play", False, self.color_white).convert()
+                self.screen.blit(play_text, play_text.get_rect(center=(play_button.centerx, play_button.centery)))
+                # Quit button and text
+                pg.draw.rect(self.screen, self.color_dark_grey, quit_button, border_radius=15)
+                if quit_button.collidepoint(pg.mouse.get_pos()):
+                    quit_text = self.font_50.render("Quit", False, self.color_red).convert()
+                else:
+                    quit_text = self.font_50.render("Quit", False, self.color_white).convert()
+                self.screen.blit(quit_text, quit_text.get_rect(center=(quit_button.centerx, quit_button.centery)))
+                # Logo
+                self.screen.blit(self.logo_small, self.logo_small.get_rect(center=((self.window_width / 2) - 155, 92)))
+                # Title text
+                self.screen.blit(self.title_text, (540, 70))
+                # Credit text
+                self.screen.blit(self.credit_text, (300, 650))
+                # PyGame Render
+                pg.display.update()
+                self.clock.tick(self.fps)
 
     def pause(self):
         paused_text = self.font_50.render("Paused", False, self.color_black).convert()
@@ -238,60 +251,78 @@ class Game:
             self.clock.tick(self.fps)
 
     def end_screen(self, victor):
-        play_button = pg.Rect((265, 375), (750, 100))
+        play_again_button = pg.Rect((265, 375), (750, 100))
         quit_button = pg.Rect((265, 500), (750, 100))
 
-        # Render
-        self.screen.fill(self.bg_color)
+        while self.state_stack[-1] == "END":
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    exit(0)
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_ESCAPE:
+                        pg.quit()
+                        exit(0)
+                elif event.type == pg.MOUSEBUTTONDOWN:
+                    if quit_button.collidepoint(pg.mouse.get_pos()):
+                        pg.quit()
+                        exit(0)
+                    elif play_again_button.collidepoint(pg.mouse.get_pos()):
+                        self.reset_game()
+                        self.state_stack.pop()
+                        self.game()
 
-        # Grass background
-        green_background = pg.Rect((0, 400), (self.screen.get_width(), 400))
-        pg.draw.rect(self.screen, self.color_green, green_background)
+            # Render
+            self.screen.fill(self.bg_color)
 
-        # Road
-        road_background = pg.Rect((340, 400), (600, 400))
-        pg.draw.rect(self.screen, self.color_black, road_background)
-        # Road side - Left
-        pg.draw.polygon(self.screen, self.color_black, ((340, 400), (340, 720), (250, 720)))
-        # Road side - Right
-        pg.draw.polygon(self.screen, self.color_black, ((940, 400), (940, 720), (1035, 720)))
-        # Road side wall - left
-        pg.draw.polygon(self.screen, self.color_red, ((340, 400), (250, 720), (210, 720), (300, 400)))
-        # Road side wall - right
-        pg.draw.polygon(self.screen, self.color_red, ((940, 400), (1035, 720), (1075, 720), (980, 400)))
+            # Grass background
+            green_background = pg.Rect((0, 400), (self.screen.get_width(), 400))
+            pg.draw.rect(self.screen, self.color_green, green_background)
 
-        # Play button and text
-        pg.draw.rect(self.screen, self.color_dark_grey, play_button, border_radius=15)
-        if play_button.collidepoint(pg.mouse.get_pos()):
-            play_text = self.font_50.render("Play again", False, self.color_red).convert()
-        else:
-            play_text = self.font_50.render("Play again", False, self.color_white).convert()
-        self.screen.blit(play_text, play_text.get_rect(center=(play_button.centerx, play_button.centery)))
-        # Quit button and text
-        pg.draw.rect(self.screen, self.color_dark_grey, quit_button, border_radius=15)
-        if quit_button.collidepoint(pg.mouse.get_pos()):
-            quit_text = self.font_50.render("Quit", False, self.color_red).convert()
-        else:
-            quit_text = self.font_50.render("Quit", False, self.color_white).convert()
-        self.screen.blit(quit_text, quit_text.get_rect(center=(quit_button.centerx, quit_button.centery)))
-        # Logo
-        self.screen.blit(self.logo_small, self.logo_small.get_rect(center=((self.window_width / 2) - 155, 92)))
-        # Title text
-        self.screen.blit(self.title_text, (540, 70))
-        # Winner text
-        victor_text = self.font_50.render("And the winner is...", False, self.color_black).convert()
-        self.screen.blit(victor_text, (380, 200))
-        if victor == "Player":
-            victor_result_text = self.font_100.render("The Player", False, self.color_red).convert()
-            self.screen.blit(victor_result_text, (340, 250))
-        else:
-            victor_result_text = self.font_100.render("The Computer", False, self.color_red).convert()
-            self.screen.blit(victor_result_text, (230, 250))
-        # Credit text
-        self.screen.blit(self.credit_text, (300, 650))
-        # PyGame Render
-        pg.display.update()
-        self.clock.tick(self.fps)
+            # Road
+            road_background = pg.Rect((340, 400), (600, 400))
+            pg.draw.rect(self.screen, self.color_black, road_background)
+            # Road side - Left
+            pg.draw.polygon(self.screen, self.color_black, ((340, 400), (340, 720), (250, 720)))
+            # Road side - Right
+            pg.draw.polygon(self.screen, self.color_black, ((940, 400), (940, 720), (1035, 720)))
+            # Road side wall - left
+            pg.draw.polygon(self.screen, self.color_red, ((340, 400), (250, 720), (210, 720), (300, 400)))
+            # Road side wall - right
+            pg.draw.polygon(self.screen, self.color_red, ((940, 400), (1035, 720), (1075, 720), (980, 400)))
+
+            # Play button and text
+            pg.draw.rect(self.screen, self.color_dark_grey, play_again_button, border_radius=15)
+            if play_again_button.collidepoint(pg.mouse.get_pos()):
+                play_text = self.font_50.render("Play again", False, self.color_red).convert()
+            else:
+                play_text = self.font_50.render("Play again", False, self.color_white).convert()
+            self.screen.blit(play_text, play_text.get_rect(center=(play_again_button.centerx, play_again_button.centery)))
+            # Quit button and text
+            pg.draw.rect(self.screen, self.color_dark_grey, quit_button, border_radius=15)
+            if quit_button.collidepoint(pg.mouse.get_pos()):
+                quit_text = self.font_50.render("Quit", False, self.color_red).convert()
+            else:
+                quit_text = self.font_50.render("Quit", False, self.color_white).convert()
+            self.screen.blit(quit_text, quit_text.get_rect(center=(quit_button.centerx, quit_button.centery)))
+            # Logo
+            self.screen.blit(self.logo_small, self.logo_small.get_rect(center=((self.window_width / 2) - 155, 92)))
+            # Title text
+            self.screen.blit(self.title_text, (540, 70))
+            # Winner text
+            victor_text = self.font_50.render("And the winner is...", False, self.color_black).convert()
+            self.screen.blit(victor_text, (380, 200))
+            if victor == "Player":
+                victor_result_text = self.font_100.render("The Player", False, self.color_red).convert()
+                self.screen.blit(victor_result_text, (340, 260))
+            else:
+                victor_result_text = self.font_100.render("The Computer", False, self.color_red).convert()
+                self.screen.blit(victor_result_text, (230, 260))
+            # Credit text
+            self.screen.blit(self.credit_text, (300, 650))
+            # PyGame Render
+            pg.display.update()
+            self.clock.tick(self.fps)
 
     def game(self):
         ui_tire_health_20 = pg.Rect((15, 45), (40, 60))
@@ -312,7 +343,7 @@ class Game:
         ui_current_lap_player_text = self.font_24.render("Player", False, self.color_white).convert()
         ui_current_lap_computer_text = self.font_24.render("PC", False, self.color_red).convert()
 
-        ui_tire_swap_hint = self.font_40.render("[1] Soft, [2] Medium, [3] Hard", False, self.color_white).convert()
+        ui_tire_swap_hint = self.font_30.render("[1] Soft, [2] Medium, [3] Hard", False, self.color_white).convert()
 
         ui_tire_soft = pg.transform.scale(pg.image.load("res/textures/tire_soft.png"), (64, 64))
         ui_tire_medium = pg.transform.scale(pg.image.load("res/textures/tire_medium.png"), (64, 64))
@@ -346,6 +377,10 @@ class Game:
                     pg.quit()
                     exit(0)
                 elif event.type == pg.KEYDOWN:
+                    # TODO: Remove
+                    if event.key == pg.K_q:
+                        self.state_stack.append("END")
+                        self.end_screen("Computer")
                     if event.key == pg.K_ESCAPE:
                         # Show pause menu
                         self.state_stack.append("PAUSE")
@@ -538,7 +573,7 @@ class Game:
 
             # Show the tire swap hint
             if self.player.can_pit:
-                self.screen.blit(ui_tire_swap_hint, (310, 457))
+                self.screen.blit(ui_tire_swap_hint, (390, 460))
 
             # Pit timer text
             if self.pit_timer > 0.0:
